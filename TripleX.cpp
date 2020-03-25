59db0bf738678d132de897ab2b50bd532ae15c9b
Dave Calderwood <davidhcalderwood@gmail.com>
2020-03-25 10:21:39 -0400

    variables

--- TripleX.cpp
+++ TripleX.cpp
@@ -4,5 +4,16 @@ int main() {
     std::cout << "You are a secret agent breaking into a secure server room.";
     std::cout << std::endl;
     std::cout << "You need to enter the correct code to continue...";
+    std::cout << std::endl;
+
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+    int sum = a + b + c;
+
+    std::cout << std::endl;
+    std::cout << sum;
+
     return 0;
 }

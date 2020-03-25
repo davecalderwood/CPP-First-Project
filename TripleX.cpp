bc367b08dd55c665993e8143c1782a57b4ae0a98
Dave Calderwood <davidhcalderwood@gmail.com>
2020-03-25 09:32:51 -0400

    Printing strings on new lines

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World";
+    std::cout << "You are a secret agent breaking into a secure server room.";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct code to continue...";
     return 0;
 }
